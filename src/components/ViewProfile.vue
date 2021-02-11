68414c6ad68d128e14656947f3717f63f9240a0a
Roger Ccopa <rogerccopa@gmail.com>
2021-02-10 19:28:33 -0500

    Route parameters

---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="view-profile">
+        <h2>Profile for {{userId}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ViewProfile',
+    data(){
+        return {
+            // $route refers to the current route
+            userId: this.$route.params.user_id
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
