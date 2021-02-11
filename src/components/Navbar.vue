75a5c6edf3ea6d2a792d98f6aee8d4270a31855a
Roger Ccopa <rogerccopa@gmail.com>
2021-02-10 20:53:08 -0500

    Router links more

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,6 +4,13 @@
             <li><router-link to="/">Home</router-link></li>
             <li><router-link :to="{name:'About'}">About</router-link></li>
         </ul>
+        <ul>
+            <li v-for="(id,index) in ids" :key="index">
+                <router-link :to="{name:'ViewProfile', params:{user_id:id}}">
+                    <span>Profile {{id}}</span>
+                </router-link>    
+            </li>
+        </ul>
     </nav>
 </template>
 
@@ -12,7 +19,7 @@ export default {
     name: 'Navbar',
     data(){
         return {
-
+            ids: ['1','2','3','4']
         }
     }
 }
