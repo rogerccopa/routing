fa72fdcba203cb6dcb7bb24ace9235788612f186
Roger Ccopa <rogerccopa@gmail.com>
2021-02-10 22:21:47 -0500

    Programmatically Redirecting Users

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,6 +11,18 @@
                 </router-link>    
             </li>
         </ul>
+        <h2>Navigation Controls</h2>
+        <ul>
+            <li>
+                <button @click="goBack">Go Back</button>
+            </li>
+            <li>
+                <button @click="goHome">Redirect to Home</button>
+            </li>
+            <li>
+                <button @click="goForward">Go Forward</button>
+            </li>
+        </ul>
     </nav>
 </template>
 
@@ -20,6 +32,19 @@ export default {
     data(){
         return {
             ids: ['1','2','3','4']
+        }
+    },
+    methods:{
+        goHome(){
+            // $route: refers to current route
+            // $router: refers to the entire app router n keeps our nav history
+            this.$router.push({name:'Home'})
+        },
+        goBack(){
+            this.$router.go(-1);
+        },
+        goForward(){
+            this.$router.go(1);
         }
     }
 }
@@ -32,5 +57,9 @@ ul{
 }
 a{
     color: #42b983;
+}
+li{
+    display: inline-block;
+    margin: 10px;
 }
 </style>
